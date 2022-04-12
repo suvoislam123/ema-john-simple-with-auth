d6d019c25fbe51d9d8faf232dd8c8e74f71bad47
suvoislam123 <suvoislam753@gmail.com>
2022-04-12 23:14:36 +0600

    added to git hub

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,17 +5,15 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCk7iTgiGpzXvhjitNRf3Oa1EqkKZqBzNo",
-  authDomain: "ema-john-simple-85c01.firebaseapp.com",
-  projectId: "ema-john-simple-85c01",
-  storageBucket: "ema-john-simple-85c01.appspot.com",
-  messagingSenderId: "598631523012",
-  appId: "1:598631523012:web:16797e70a7e5e9f0acdf91"
+  apiKey: "AIzaSyCD6iHadyIM4HnxVZSjvgXfT1e0JzX4mq0",
+  authDomain: "emajhon-simple-479fd.firebaseapp.com",
+  projectId: "emajhon-simple-479fd",
+  storageBucket: "emajhon-simple-479fd.appspot.com",
+  messagingSenderId: "1078062224106",
+  appId: "1:1078062224106:web:6cc261359d645c017ffd93"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
+const auth = getAuth(app)
 export default auth;
